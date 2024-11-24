3fcc2631de5c1758991a9a31747a5644d4b97166
piyush <piyush72717272@gmail.com>
2024-11-24 13:11:14 +0530

    store ui fixes

--- lib/features/shop/screens/store/store_screen.dart
+++ lib/features/shop/screens/store/store_screen.dart
@@ -2,9 +2,12 @@ import 'package:bouncy_bargains/common/widgets/appbar/appbar.dart';
 import 'package:bouncy_bargains/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:bouncy_bargains/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:bouncy_bargains/common/widgets/images/x_circular_image.dart';
+import 'package:bouncy_bargains/common/widgets/layouts/grid_layout.dart';
 import 'package:bouncy_bargains/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:bouncy_bargains/common/widgets/texts/section_heading.dart';
+import 'package:bouncy_bargains/common/widgets/texts/x_brand_title_text_with_verified_icon.dart';
 import 'package:bouncy_bargains/utils/constants/colors.dart';
+import 'package:bouncy_bargains/utils/constants/enums.dart';
 import 'package:bouncy_bargains/utils/constants/image_strings.dart';
 import 'package:bouncy_bargains/utils/constants/sizes.dart';
 import 'package:bouncy_bargains/utils/helpers/helper_functions.dart';
@@ -63,32 +66,58 @@ class StoreScreen extends StatelessWidget {
                         height: XSizes.spaceBtwItems / 1.5,
                       ),
 
-                      XRoundedContainer(
-                        padding: const EdgeInsets.all(XSizes.sm),
-                        showBorder: true,
-                        backgroundColor: Colors.transparent,
-                        child: Row(
-                          children: [
-                            // Icon
-                            XCircularImage(
-                              backgroundColor: Colors.transparent,
-                              image: XImages.clothIcon,
-                              overlayColor:
-                                  dark ? XColors.white : XColors.black,
-                            ),
-                            SizedBox(
-                              width: XSizes.spaceBtwItems / 2,
-                            ),
+                      XGridLayout(
+                          itemCount: 4,
+                          mainAxisExtent: 80,
+                          itemBuilder: (_, index) {
+                            return GestureDetector(
+                              onTap: () {},
+                              child: XRoundedContainer(
+                                padding: const EdgeInsets.all(XSizes.sm),
+                                showBorder: true,
+                                backgroundColor: Colors.transparent,
+                                child: Row(
+                                  children: [
+                                    // Icon
+                                    Flexible(
+                                      child: XCircularImage(
+                                        backgroundColor: Colors.transparent,
+                                        image: XImages.clothIcon,
+                                        overlayColor: dark
+                                            ? XColors.white
+                                            : XColors.black,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: XSizes.spaceBtwItems / 2,
+                                    ),
 
-                            // Text
-                            Column(
-                              children: [
-
-                              ],
-                            )
-                          ],
-                        ),
-                      )
+                                    // Text
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const XBrandTitleWithVerifiedIcon(
+                                            title: "Nike",
+                                            brandTextSize: TextSizes.large,
+                                          ),
+                                          Text(
+                                            '256 products',
+                                            overflow: TextOverflow.ellipsis,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .labelMedium,
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          })
                     ],
                   ),
                 ),
